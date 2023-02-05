d9ae9ee24006cf928b6523074d5f6b1e0ad377d7
leestephen0320 <leestephen0320@gmail.com>
2023-02-05 22:32:43 +0000

    modified:   src/main/java/SetAllArrayElements.java

--- src/main/java/SetAllArrayElements.java
+++ src/main/java/SetAllArrayElements.java
@@ -24,7 +24,7 @@ public class SetAllArrayElements {
      */
     public void set(int[] arr, int n){
         for(int i = 0; i < arr.length; i++){
-            arr[i] = 0;
+            arr[i] = n;
         }
     }
 }
